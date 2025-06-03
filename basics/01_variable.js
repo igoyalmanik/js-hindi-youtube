cc4872685f6345ad2cf0939b4710cc13906e511e
igoyalmanik <ritugoyal5989@gmail.com>
2025-06-03 11:25:30 +0000

    var,datatypes

---
+++ basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountID = 123
+let accountEmail = "mani@google.com"
+var accpass = "12345"
+accountCity = "Jaipur"
+let abc
+
+console.table( [accountID,accountCity,accountEmail,accpass,abc]);
---
+++ basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"Use strict"; //it treat all js code as newer version
+
+// alert(3 + 3) : we are using nodejs, not browser
+
+console.log(3+
+    3) // code readability should be high
+
+console.log("Manik")
+
+//typeof
+
+console.log(typeof null)
+
+//const abc; we cant declare a variable with undefined value with keyword const.
