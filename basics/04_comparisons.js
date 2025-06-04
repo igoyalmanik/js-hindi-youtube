8cf23844966e7daa92a245d25b480df6f3c569b6
igoyalmanik <ritugoyal5989@gmail.com>
2025-06-04 09:43:27 +0000

    comparisons

---
+++ basics/04_comparisons.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0) // actually equality operator (==) and comparison operators >=,<=,<,> works differently 
+// the comparison operator converts the null into zero value(0).
+
+
+// console.log(undefined > 0)
+// console.log( undefined <  0)
+// console.log( undefined ==  0)
+
+console.log("2"===2) // === it is called strict check, in this it alse checks the datatype,
+// == meanwhile in this we convert the datatype, but here === we dont convert it
