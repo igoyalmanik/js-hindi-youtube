e3adea376ce83b4a049f77c1b7cdce0fca9abb18
igoyalmanik <ritugoyal5989@gmail.com>
2025-06-05 18:26:58 +0000

    stack-heap

---
+++ basics/05_stack-heap.js
@@ -0,0 +1,19 @@
+let myyoutubename = "hiteshchoudhary.com"
+let anothername = myyoutubename
+
+anothername = "ChaiAurCode"
+
+console.log(myyoutubename)
+console.log(anothername)
+
+let userone = {
+    email : "user@google.com",
+    upi : "user@ygl"
+}
+
+let userTwo = userone
+
+userTwo.email = "hari@gmail.com"
+
+console.log(userone.email)
+console.log(userTwo)
