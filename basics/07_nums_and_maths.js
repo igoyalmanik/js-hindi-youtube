eee001443c1626a66743cd3049ea5c9afefe358c
igoyalmanik <ritugoyal5989@gmail.com>
2025-06-06 10:14:41 +0000

    math_and_numbers

---
+++ basics/07_nums_and_maths.js
@@ -0,0 +1,53 @@
+// const score = 100;
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+// const othernum = 123.89
+// console.log((othernum.toPrecision(4)));
+
+// const othernum2 = 1231.89
+// console.log((othernum2.toPrecision(4)));
+
+// const othernum3 = 1231.89
+// console.log((othernum3.toPrecision(5)));
+
+// const othernum4 = 1231.89
+// console.log((othernum4.toPrecision(3)));
+
+
+// const hundreds1 = 100000000
+// console.log(hundreds1);
+
+// const hundreds2 = 100000000
+// console.log(hundreds2.toLocaleString());
+
+
+// const hundreds3 = 100000000
+// console.log(hundreds3.toLocaleString('en-IN'));
+
+//     ******       MATHS       ******* //
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.09))
+// console.log(Math.floor(4.99));
+
+// console.log(Math.min(4,6,2,8,1));
+// console.log(Math.max(2,5,7,9,34));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+console.log(Math.floor((Math.random()*10) + 1));
+
+const max = 20;
+const min = 10;
+
+console.log((Math.floor(Math.random()* (max - min +1))) + min )
