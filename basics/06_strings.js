5bab45c1aeb9bf25908f1437875eaf3d4cccd716
igoyalmanik <ritugoyal5989@gmail.com>
2025-06-06 09:06:19 +0000

    Strings

---
+++ basics/06_strings.js
@@ -0,0 +1,43 @@
+const name = "hitesh"
+const repoCount = 50;
+
+ // console.log(name + repoCount + " Value")
+
+ console.log(`hello my name is ${name} and my repocount is ${repoCount}`)
+
+ const gamename = new String(`hiteshhc-manik-krn`)
+
+//  console.log(gamename[0])
+//  console.log(gamename.__proto__) // {} it means object.
+
+//  console.log(gamename.length)
+
+//  console.log(gamename.charAt(2))
+
+//  console.log(gamename.toUpperCase());
+
+//  console.log(gamename.indexOf('t'))
+
+ const newString = gamename.substring(0,4)
+ console.log(newString);
+
+ const anotherstring = gamename.slice(-7,2)
+ console.log(anotherstring);
+ 
+ const newstringone = "   hitesh   "
+ console.log(newstringone);
+ console.log(newstringone.trim());
+
+ const url = "https:hitesh60/hitesh%20ind"
+
+ console.log(url.replace('%20','-'));
+
+ console.log(url.includes('hitesh'))
+
+ console.log(gamename.split('-'));
+ 
+ 
+ 
+ 
+ 
+ 
