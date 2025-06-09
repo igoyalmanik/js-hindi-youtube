dd953c640ffbea979d490f0d3d65433d133cd6fc
igoyalmanik <ritugoyal5989@gmail.com>
2025-06-09 10:26:55 +0000

    jason talk and object de-structuring

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -21,20 +21,20 @@ const regularuser = {
 
 }
 
- console.log(regularuser.fullname);
+//  console.log(regularuser.fullname);
 
- console.log(regularuser.fullname.userfullname);
+//  console.log(regularuser.fullname.userfullname);
 
-console.log(regularuser.fullname?.userfullname);  
+// console.log(regularuser.fullname?.userfullname);  
 
-const obj1 = {1:"a" , 2: " b"}
-const obj2 = {3: "a", 4:"b"}
-const obj4 = {5: "a", 6:"b"}
+// const obj1 = {1:"a" , 2: " b"}
+// const obj2 = {3: "a", 4:"b"}
+// const obj4 = {5: "a", 6:"b"}
 
-//const obj3 = {obj1,obj2}
-//const obj = Object.assign({},obj1,obj2,obj4) // target and source.
-const obj = {...obj1,...obj2}
-console.log(obj);
+// //const obj3 = {obj1,obj2}
+// //const obj = Object.assign({},obj1,obj2,obj4) // target and source.
+// const obj = {...obj1,...obj2}
+// console.log(obj);
 
 const users = [
     
@@ -51,11 +51,45 @@ const users = [
 
     users[1].email
 
-    console.log(tinderuser2);
+    // console.log(tinderuser2);
     
 
-    console.log(Object.keys(tinderuser2))
-     console.log(Object.values(tinderuser2))
-    console.log(Object.entries(tinderuser2)) 
+    // console.log(Object.keys(tinderuser2))
+    //  console.log(Object.values(tinderuser2))
+    // console.log(Object.entries(tinderuser2)) 
 
-    console.log(tinderuser2.hasOwnProperty('isLoggedin'))
+    // console.log(tinderuser2.hasOwnProperty('isLoggedin'))
+
+    const course = {
+        coursename: "js in hindi",
+        price: "999",
+        courseInstructor : "hitesh"
+    }
+
+   // course.courseInstructor
+
+   const {courseInstructor} = course
+   console.log(courseInstructor)
+
+
+ //  ---- object de-structuring
+//    const navbar = ({company}) => {
+
+//    }
+
+//    navbar(company = "hitesh")
+
+// API -> JUST GIVE YOUR WORK TO ANOTHER PERSON.
+// object with dont have any name is called as jason. , they can also be in form of arrays.
+// in form of object
+// {
+//    name:"hitesh",
+//    coursename: "js in hindi",
+//    price : "free"
+// }
+//in form of arrays
+// [
+//     {},
+//     {},
+//     {}
+// ]
