ee885206bea3267b4bf83acfc859e989eb02a800
igoyalmanik <ritugoyal5989@gmail.com>
2025-06-14 06:54:43 +0000

    for/while/do-while

---
+++ 05_iterations/while.js
@@ -0,0 +1,31 @@
+//while
+
+let index = 0;
+while(index<=10)
+{
+  //console.log(`value of index is ${index}`);
+  index = index+2;
+}
+
+let myArray = ["Camerica","dom",'rock'];
+let arr=0;
+while(arr<myArray.length){
+    console.log(
+        myArray[arr]
+    );
+
+    arr++;
+}
+
+
+// let score = 1
+// do {
+//    console.log(`Score is ${score}`);
+//    score++;
+// }while(score<20)
+
+let score = 11
+do {
+   console.log(`Score is ${score}`);
+   score++;
+}while(score<10)
